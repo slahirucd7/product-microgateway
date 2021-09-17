7e9f5062878883d497919026006ee1342d365e7f
slahirucd7 <slahiruc007@gmail.com>
2021-09-17 19:52:52 +0530

    initial implementation

---
+++ enforcer/src/main/java/org/wso2/choreo/connect/enforcer/security/jwt/APIKeyHandler.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.choreo.connect.enforcer.security.jwt;
+
+import org.wso2.choreo.connect.enforcer.api.RequestContext;
+import org.wso2.choreo.connect.enforcer.config.ConfigHolder;
+import org.wso2.choreo.connect.enforcer.security.Authenticator;
+
+/**
+ * An abstract class which can be used to handle API keys.
+ */
+public abstract class APIKeyHandler implements Authenticator {
+
+    @Override
+    public boolean canAuthenticate(RequestContext requestContext) {
+        String internalKey = requestContext.getHeaders().get(
+                ConfigHolder.getInstance().getConfig().getAuthHeader().getTestConsoleHeaderName().toLowerCase());
+        if (internalKey != null && internalKey.split("\\.").length == 3) {
+            return true;
+        }
+        return false;
+    }
+}
+
+//eyJ4NXQiOiJOVGRtWmpNNFpEazNOalkwWXpjNU1tWm1PRGd3TVRFM01XWXdOREU1TVdSbFpEZzROemM0WkE9PSIsImtpZCI6ImdhdGV3YXlfY2VydGlmaWNhdGVfYWxpYXMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbkBjYXJib24uc3VwZXIiLCJhcHBsaWNhdGlvbiI6eyJvd25lciI6ImFkbWluIiwidGllclF1b3RhVHlwZSI6bnVsbCwidGllciI6IlVubGltaXRlZCIsIm5hbWUiOiJEZWZhdWx0QXBwbGljYXRpb24iLCJpZCI6MSwidXVpZCI6IjNiODE3YTUzLTA2MDktNGM2Ny04YWZiLWNiOWRkYzVlMWZjYiJ9LCJpc3MiOiJodHRwczpcL1wvYXBpbTo5NDQzXC9vYXV0aDJcL3Rva2VuIiwidGllckluZm8iOnt9LCJrZXl0eXBlIjoiUFJPRFVDVElPTiIsInBlcm1pdHRlZFJlZmVyZXIiOiIiLCJzdWJzY3JpYmVkQVBJcyI6W10sInRva2VuX3R5cGUiOiJhcGlLZXkiLCJwZXJtaXR0ZWRJUCI6IiIsImlhdCI6MTYzMTg3MjY2NSwianRpIjoiOTUwNGJjOWUtMWIyOS00MzYxLTkyY2EtNjZkYzIyZjk2MDQwIn0=.IZWQe0YGD0UPyunMdi1BHQglE71svOIpQI-IwF5PYoV2r4HPWONpPG8jZO5KXIz-nGAyMNOmqQxS1Nay3J3TpJsCn7IrCx1mWNpU6qO9Gjc--nkGTyCdMRbydfhDJdetSleUuk0P00vfgKqftrZpx8zTybugGFFW10PIEn9gjWhlON8phw46T9du57p8cOh3fDeyq7SlY20QyxUybD1T7eTwiYGF3XyS7jofohMU1ifY1_fZddwipdNHeHBHSW34RRr7BI0EXXA8HOJwnstRQl5P8XEn1kJoHGoBM8iK-O_4pBemaw7Z5bK5rwK4o7yZ3OTsYogPyolnOCI6iUZNmw==
