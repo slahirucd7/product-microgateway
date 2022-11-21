656db194df90cf09f1da1e03ac745c39e6b03b16
slahirucd7 <slahiruc007@gmail.com>
2022-11-21 18:16:35 +0530

    rl test case addition

--- adapter/internal/oasparser/envoyconf/http_filters.go
+++ adapter/internal/oasparser/envoyconf/http_filters.go
@@ -62,7 +62,7 @@ func getHTTPFilters() []*hcmv3.HttpFilter {
 		router,
 	}
 	conf, _ := config.ReadConfigs()
-	if conf.Envoy.RateLimit.Enable {
+	if conf.Envoy.RateLimit.Enabled {
 		rateLimit := getRateLimitFilter()
 		httpFilters = httpFilters[:len(httpFilters)-1]
 		httpFilters = append(httpFilters, rateLimit)
